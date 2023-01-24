2330f8e6d07ef8a529ffd6d4dc6c44dc841a7d08
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-24 17:00:02 +0530

    downloadable link func created

--- routes/pagesRoutes.js
+++ routes/pagesRoutes.js
@@ -1,8 +1,25 @@
-const router = require("express").Router()
+const router = require("express").Router();
 
+const File = require("../models/file");
+require("dotenv").config();
+const BASE_URL = process.env.BASE_URL;
 
+router.get("/download-page/:uuid", async (req, res) => {
+  try {
+    const file = await File.findOne({ uuid: req.params.uuid });
 
-router.post()
+    if (!file) {
+      return res.status(400).render("downloads", { error: "LInk expired" });
+    }
+    return res.status(200).render("downloads", {
+      uuid: file.uuid,
+      fileName: file.fileName,
+      fileSize: file.size,
+      downloadLink: `${BASE_URL}/api/files/download/${file.uuid}`,
+    });
+  } catch (error) {
+    return res.status(400).render("downloads", { error: error });
+  }
+});
 
-
-module.exports = router
+module.exports = router;
