7e4b489a807668a4747deff5ab09a85411cc0c1d
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-30 13:35:03 +0530

    mongo db url directly added

--- config/db.js
+++ config/db.js
@@ -1,43 +1,21 @@
 const mongoose = require("mongoose");
-
 require("dotenv").config();
 
-const MONGO_DB_URL = process.env.MONGO_URL;
-mongoose.set("strictQuery", true);
-
+const MONGO_URL = process.env.MONGO_URL;
 const MONGO_OPTIONS = {
-  useNewUrlParser: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_DB_URL, MONGO_OPTIONS);
-    console.log("connected to DB");
-  } catch (error) {
-    console.log(error, "Db Not Connected");
+    await mongoose.connect("mongodb+srv://nikhilpn:aG4HT4vDJmuNr9DI@cluster0.uh3by3j.mongodb.net/?retryWrites=true&w=majority", MONGO_OPTIONS); // Database connection 🥳
+    console.log("Database Connected 🥳🥳🥳🥳");
+  } catch (err) {
+    console.log("Could not connect to MongoDB");
+    console.log(err);
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
-
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// const MONGO_URL = process.env.MONGO_URL;
-// const MONGO_OPTIONS = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(MONGO_URL, MONGO_OPTIONS); // Database connection 🥳
-//     console.log("Database Connected 🥳🥳🥳🥳");
-//   } catch (err) {
-//     console.log("Could not connect to MongoDB");
-//     console.log(err);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
